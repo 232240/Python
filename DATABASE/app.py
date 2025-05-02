30f0e8494baa7667f68188d8366ac429a693e71a
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-02 12:42:37 +1200

    Tidied up

--- DATABASE/app.py
+++ DATABASE/app.py
@@ -2,7 +2,7 @@ import sqlite3
 
 db = sqlite3.connect("SQL_Practice_Assesment_Task7.db")
 cursor = db.cursor()
-sql = "SELECT * FROM Card"
+sql = "SELECT * from Card;"
 cursor.execute(sql)
 results = cursor.fetchall()
 print(results)
