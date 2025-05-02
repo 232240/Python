45fdef6d7f0976967ba152adf0f662a9b30170d2
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-02 12:30:57 +1200

    Mid way backup

--- DATABASE/app.py
+++ DATABASE/app.py
@@ -1,1 +1,9 @@
-print("Hello, World")
+import sqlite3
+
+db = sqlite3.connect("SQL_Practice_Assesment_Task7.db")
+cursor = db.cursor()
+sql = "SELECT * FROM Card"
+cursor.execute(sql)
+results = cursor.fetchall()
+print(results)
+db.close()
