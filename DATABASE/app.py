f704263a4c5aeb5fb1fd08211cb8144c441ff685
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-05-06 12:42:14 +1200

    Finished practice assesment!

--- DATABASE/app.py
+++ DATABASE/app.py
@@ -19,14 +19,43 @@ def print_all_cards():
         print(f"{card[1]:<30}{card[2]:<20}{card[3]:<20}")
     #loop finish here
     db.close()
-
+def print_all_cards_by_rarity():
+    '''print all the cards by rarity'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from Card ORDER BY Rarity DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print("    Name                       HP                   Rarity")
+    for card in results:
+        print(f"{card[1]:<30}{card[2]:<20}{card[3]:<20}")
+    #loop finish here
+    db.close()
+def print_all_cards_by_hp():
+    '''print all the cards by hp'''
+    db = sqlite3.connect(DATABASE)
+    cursor = db.cursor()
+    sql = "SELECT * from Card ORDER BY hp DESC;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    #loop through all the results
+    print("    Name                       HP                   Rarity")
+    for card in results:
+        print(f"{card[1]:<30}{card[2]:<20}{card[3]:<20}")
+    #loop finish here
+    db.close()
 #main code
 while True:
-    user_input = input("What would you like to do? \n1. Print all cards \n2. Exit")
+    user_input = input("\nWhat would you like to do?\n1. Print all cards\n2. All cards by Rarity\n3. Show cards sorted by HP\n4. Exit\n")
     if user_input == "1":
-        print_all_cards()
-    if user_input == "2":
-        break
+       print_all_cards()
+    elif user_input == "2":
+       print_all_cards_by_rarity()
+    elif user_input == "3":
+       print_all_cards_by_hp()
+    elif user_input == "4":
+       break
     else:
-        print("That was not an option.")
+        print("That was not an option\n")
         
